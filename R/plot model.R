2cb7c3af83400197be8fb38067be71bd8411ed3b
rosseji <rosseireland@gmail.com>
2018-01-10 09:28:53 +1100

    fix comp plot names

--- R/plot model.R
+++ R/plot model.R
@@ -3,15 +3,6 @@
 
 plot_model <- function(fc_ls, comp_ls, prod_cat, replication_id, rmse_ls) {
 
-  print(is_empty(rmse_ls[["rmse_med"]]))
-  print(is_empty(rmse_ls[["rmse_mean"]]))
-
-  print(is_empty(fc_ls[["plot"]]))
-  print(is_empty(comp_ls[["seasonal.24.1"]][["plot"]]))
-  print(is_empty(comp_ls[["seasonal.168.1"]][["plot"]]))
-  print(is_empty(comp_ls[["seasonal.168.1"]][["plot"]]))
-  print(is_empty(comp_ls[["regression"]][["plot"]]))
-
 
   print(paste0("Model ", replication_id, " ", prod_cat, "  ---- RMSE med:", round(rmse_ls[["rmse_med"]]), " mean:", round(rmse_ls[["rmse_mean"]])))
 
@@ -19,7 +10,7 @@ plot_model <- function(fc_ls, comp_ls, prod_cat, replication_id, rmse_ls) {
     grobs = list(fc_ls[["plot"]] + ggtitle(paste0("Model ", replication_id, " ", prod_cat, " RMSE med:", round(rmse_ls[["rmse_med"]]), " mean:", round(rmse_ls[["rmse_mean"]]))),
                  comp_ls[["trend"]][["plot"]],
                  comp_ls[["seasonal.24.1"]][["plot"]],
-                 comp_ls[["seasonal.168.1"]][["plot"]],
+                 comp_ls[["seasonal.7.24"]][["plot"]],
                  comp_ls[["regression"]][["plot"]]),
     widths = c(1, 1),
     layout_matrix = rbind(c(1, 1),
