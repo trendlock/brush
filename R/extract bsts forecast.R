d4828d425e35e5372c159951cf070b6afcebb25f
rosseji <rosseireland@gmail.com>
2018-01-22 16:46:41 +1100

    bump move to master

--- R/extract bsts forecast.R
+++ R/extract bsts forecast.R
@@ -8,7 +8,6 @@ extract_bstb_forecast <- function(pred_obj, next_periods) {
     mutate(key = "Forecast (Mean)",
            x.id = row_number())
 
-
   df_med <- pred_obj$median %>%
     enframe() %>%
     mutate(key = "Forecast (Median)",
@@ -34,50 +33,29 @@ extract_bstb_forecast <- function(pred_obj, next_periods) {
    inner_join(next_periods, by = "bin_id")
 
  fc_df <- fc_df %>%
-   dplyr::select(bin_id, key, value, Total, AIR_TEMP, PRCP) %>%
+   dplyr::select(bin_id, key, value, Total) %>%
    spread(key, value)
 
- weather_df <- fc_df %>%
-   dplyr::select(bin_id, AIR_TEMP, PRCP) %>%
-   gather(cat, val, -bin_id)
-
-
- fc_df <- fc_df %>%
-   dplyr::select(-AIR_TEMP, -PRCP)
  # root mean square error (for mean and median forecast)
 
- df_err <- fc_df %>%
-   mutate(diff.mean = Total - `Forecast (Median)`,
-          diff.med = Total - `Forecast (Mean)`)
-
- rmse_mean <-  sqrt(mean(pull(df_err, diff.mean)^2))
- rmse_med <-  sqrt(mean(pull(df_err, diff.med)^2))
-
+ err_vals <- fc_df %>%
+   ungroup() %>%
+   mutate(PE.med = abs(`Forecast (Median)` - Total) / Total * 100,
+          PE.mean = abs(`Forecast (Mean)` - Total) / Total * 100,
+          Error.med = abs(`Forecast (Median)` - Total),
+          Error.mean = abs(`Forecast (Mean)` - Total)) %>%
+   summarise(RMSE.med = mean(Error.med),
+             RMSE.mean = mean(Error.mean),
+             MAPE.med = mean(PE.med),
+             MAPE.mean = mean(PE.mean))
 
  fc_df <- fc_df %>%
    gather(key, value, -bin_id)
 
-
- fc_plot <- ggplot(fc_df, aes(bin_id, value, col = key, size = key, alpha = key)) +
+ fc_plot <- ggplot(fc_df, aes(as_datetime(bin_id), value, col = key)) +
    geom_line() +
-   scale_colour_manual(values = c("green", "green","orange","red","black")) +
-   scale_size_manual(values = c(1, 1, 1, 1.2, 1.2, 1, 1.5)) +
-   scale_alpha_manual(values = c(0.5, 0.5, 1, 1, 1, 1, 1)) +
    scale_x_datetime(labels=scales::date_format("%A"), date_breaks = "1 day", name = NULL)
 
- #weather plots
-
- weather_cats <- weather_df$cat %>% unique()
- weather_plots <- weather_cats %>%
-   map( ~ weather_df %>%
-          filter(cat == .x) %>%
-          ggplot(aes(x = bin_id, y = val)) +
-          geom_line() +
-          ggtitle(.x)) %>%
-   set_names(weather_cats)
-
-
-
- list(data = fc_df, plot = fc_plot, weather_plots = weather_plots, rmse_ls = list(rmse_med = rmse_med, rmse_mean = rmse_mean))
+ list(data = fc_df, plot = fc_plot, err_vals = err_vals)
 
 }
